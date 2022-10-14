ae6406be458a4fb59ef83fed115056cdbd6d63d1
KhallylReisFonseca <khallylreis@gmail.com>
2022-10-14 11:42:18 -0300

    ex002

---
+++ Fundamentos/Introducao-a-JavaScript/JavaScript-Primeiros-passos/ex002/js.js
@@ -0,0 +1,9 @@
+const base = 5;
+
+const heigth = 8;
+
+let area = base * heigth;
+
+let perimeter = base + heigth;
+
+console.log(`Base = ${base}, Altura = ${heigth}. Area = ${area} e Perimetro = ${perimeter}.`)
