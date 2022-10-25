f6ed17f906aa82d0408a28fae19d56a3442ce4d1
KhallylReisFonseca <khallylreis@gmail.com>
2022-10-25 20:48:06 -0300

    arquivos do 1 exercicio

---
+++ Fundamentos/JavaScript-DOM-Eventos-e-Web-Storage/JavaScript-Trabalhando-com-elementos/Buscando-elementos/script.js
@@ -0,0 +1,10 @@
+/*
+    Acesse o elemento elementoOndeVoceEsta.
+    Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+    Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
+    Acesse o primeiroFilho a partir de pai.
+    Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
+    Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
+    Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
+    Agora acesse o terceiroFilho a partir de pai.
+*/
