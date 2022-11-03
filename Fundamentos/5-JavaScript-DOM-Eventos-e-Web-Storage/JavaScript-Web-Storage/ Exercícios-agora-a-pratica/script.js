a53720a0e69f7fbfbd460de58c66a053418229a8
KhallylReisFonseca <khallylreis@gmail.com>
2022-11-03 09:04:14 -0300

    finalizando branch

---
+++ Fundamentos/5-JavaScript-DOM-Eventos-e-Web-Storage/JavaScript-Web-Storage/ Exercícios-agora-a-pratica/script.js
@@ -0,0 +1,18 @@
+// VARIAVEIS GLOBAIS
+const pegaBody = document.getElementsByTagName('body');
+
+
+ const criaP = document.createElement('p');
+ pegaBody[0].appendChild(criaP);
+ criaP.innerHTML = 'AAAAAAAAAAA'
+ console.log(pegaBody)
+
+
+
+
+
+
+
+
+
+// CHAMA DE FUNÇÕES
