39a6dac2141496f8814e9244e58b18493e4ce4b9
KhallylReisFonseca <khallylreis@gmail.com>
2022-11-29 16:45:59 -0300

    subindo arquvios exercicios/8.1/

---
+++ Fundamentos/8-Higher-Order-Functions-do-JavaScript-ES6/JavaScript-ES6-Higher-Order-Functions-forEach-find-some-every-sort/Array.find/js.js
@@ -0,0 +1,20 @@
+/* Para fixar
+Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5, caso ele exista:
+Copiar
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+// Adicione seu código aqui
+Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
+Copiar
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+// Adicione seu código aqui
+Utilize o find para encontrar a música com id igual a '31031685', caso ela exista:
+Copiar
+const musicas = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+];
+*/
+// Adicione seu código aqui
---
+++ Fundamentos/8-Higher-Order-Functions-do-JavaScript-ES6/JavaScript-ES6-Higher-Order-Functions-forEach-find-some-every-sort/Array.forEach/js.js
@@ -0,0 +1,12 @@
+/* Imagine que você esteja responsável por exibir a lista de emails de todas as pessoas que trabalham na mesma empresa que você. Para isso, você pode utilizar o forEach para apresentar os emails.
+
+Use o método forEach para exibir a lista de emails com a seguinte frase: O email ${email} está cadastrado em nosso banco de dados!.
+Copiar
+const emailListInData = [
+  'roberta@email.com',
+  'paulo@email.com',
+  'anaroberta@email.com',
+  'fabiano@email.com',
+];
+*/ 
+// Adicione seu código aqui
